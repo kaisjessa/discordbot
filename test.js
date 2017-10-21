70c9f4f1fd361471b182319e05d1db96077816ed
AndyCraig1 <andy.craig@ucc.on.ca>
2017-10-21 14:49:34 -0400

    Main Test

--- test.js
+++ test.js
@@ -5,10 +5,23 @@ client.on('ready', () => {
   console.log('I am ready!');
 });
 
+
 client.on('message', message => {
-  if (message.content === 'ping') {
-    message.reply('pong');
+  if (message.content === '--ping') {
+    message.channel.send('pang');
   }
+  if (message.content === '--exit') {
+    process.exit(0);
+  }
+  if (message.content === '--help') {
+    message.channel.send('COMMANDS:');
+    message.channel.send(' --ping : pang');
+    message.channel.send(' --exit : exit,');
+    message.channel.send(' --event : create an event');
+  }
+
 });
+
+
 
 client.login('MzcxMzU3OTA2NDkyMTk0ODI3.DM0dvg.yn6MJW5Upj_imhAz93izoEcWCw4');
